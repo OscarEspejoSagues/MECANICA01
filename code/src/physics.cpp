d548c50491dbd2ab81f4c1c57ecb3fa880de4ce0
Unknown <xxgreysoulxx@gmail.com>
2018-04-18 13:27:26 +0200

    Bulzanu

--- code/src/physics.cpp
+++ code/src/physics.cpp
@@ -12,8 +12,9 @@
 #include <glm/gtx/quaternion.hpp>
 
 
+glm::vec3 prePos;
+glm::quat preRotQuat;
 
-glm::vec3 oldPos;
 glm::vec3 Pos;
 glm::mat3 Rot;
 glm::vec3 LinMom;
@@ -23,7 +24,7 @@ glm::vec3 Vel;
 
 glm::mat4 mattrans;
 glm::vec3 gravity = {0.f, -9.81f, 0.f};
-glm::vec3 force01 = { -3.f, 4.f, 3.f }; //PEDAZO DE IDIOTAS ESTO TIENE QUE SER UN RANDOM <------------------------------------------------------------------------------------- !!! /!\  !!!
+glm::vec3 force01 = { -3.f, 6.f, 3.f }; //PEDAZO DE IDIOTAS ESTO TIENE QUE SER UN RANDOM <------------------------------------------------------------------------------------- !!! /!\  !!!
 glm::vec3 AngularVel;
 
 float mass = 4.f;
@@ -33,7 +34,7 @@ glm::vec3 FG;
 glm::mat3 ITensorInv;
 glm::mat3 IBody;
 
-const float tolerancia = 0.0001;
+const float tolerancia = 0.01;
 
 namespace Cube {
 	extern void setupCube();
@@ -44,9 +45,10 @@ namespace Cube {
 }
 
 void generateMatTrans();
-bool ColisionBox(const glm::vec3 vertex, float dt);
+bool ColisionBox(const glm::vec3 vertex, float dt, int i);
 void ApplyGravity(float dt);
-float BolzanoTime(float first, float last,glm::vec3 vertex);
+float BolzanoTime(float first, float last,glm::vec3 vertex, int i);
+glm::vec3 getVertex(float dt, int i);
 
 bool show_test_window = false;
 void GUI() {
@@ -72,7 +74,6 @@ void GUI() {
 void PhysicsInit() {
 	Cube::setupCube();
 	Pos = { 0.f,8.f,0.f };
-	oldPos = Pos;
 	IBody = { (1 / 12.f * mass * 2), 0, 0,
 			0, (1 / 12.f * mass * 2), 0,
 			0, 0, (1 / 12.f * mass * 2) };
@@ -90,8 +91,17 @@ void PhysicsUpdate(float dt) {
 
 	float t = 0;
 	for (int i = 0; i < 7; i++)
-		if (ColisionBox(oldPos + Cube::cubeVerts[i], dt))
-			t = BolzanoTime(0, dt, oldPos + Cube::cubeVerts[i]);
+		if (ColisionBox(prePos + glm::toMat3(preRotQuat) * Cube::cubeVerts[i], dt, i)) {
+			t = BolzanoTime(0, dt, prePos + glm::toMat3(preRotQuat) * Cube::cubeVerts[i], i);
+
+			if (i == 1) {
+				glm::vec3 vertex = getVertex(t, i);
+				float distantplaneY01 = (vertex.y * 1); //plano inferior
+				std::cout << distantplaneY01 << " - " << t << "       "<<(prePos + glm::toMat3(preRotQuat) * Cube::cubeVerts[i]).y <<std::endl;
+			}
+			
+		}
+			
 
 	Cube::updateCube(mattrans);
 }
@@ -108,11 +118,31 @@ void generateMatTrans() {
 		Pos.x,Pos.y,Pos.z,1.f,
 	};
 	mattrans = matPos*glm::toMat4(rotQuat);
+	
 }
 
-bool ColisionBox(const glm::vec3 preVertex, float dt)
+glm::vec3 getVertex(float dt, int i) {
+	glm::vec3 vertex;
+
+	glm::vec3 LinMomT = LinMom + dt * FG;
+	glm::vec3 AngMomT = AngMom + dt*torque;
+	torque = glm::vec3(0);
+	glm::vec3 VelT = LinMomT / mass;
+	glm::vec3 PosT = prePos + dt *	VelT;
+	ITensorInv = glm::toMat3(preRotQuat)*glm::inverse(IBody)*glm::transpose(glm::toMat3(preRotQuat));
+	AngularVel = ITensorInv*LinMomT;
+	rotQuat = glm::normalize(preRotQuat + dt*(glm::quat(0, AngularVel)*preRotQuat));
+
+	vertex = prePos + glm::toMat3(rotQuat) * Cube::cubeVerts[i];
+
+	return vertex;
+}
+
+bool ColisionBox(const glm::vec3 preVertex, float dt, int i)
 {
-	glm::vec3 vertex = preVertex; //CALUCLAR NOU VERTEX AMB ANTIC + DT <------------------------------------------------------------------------------------- !!! /!\  !!!
+	
+	glm::vec3 vertex = getVertex(dt, i);
+	
 
 	float distantplaneX01 = (vertex.x * 1) + 5; //distancia plano frontal
 	float distantplaneY01 = (vertex.y * 1); //plano inferior
@@ -120,27 +150,31 @@ bool ColisionBox(const glm::vec3 preVertex, float dt)
 	float distantplaneZ02 = (vertex.z * -1) + 5; //plano lateral izquierdo
 	float distantplaneX02 = (vertex.x * -1) + 5;//plano trasero
 	float distantplaneY02 = (vertex.y * -1) + 10; //plano superior
-
-
 																	//Rebotes planos frontales
+
 	if (distantplaneX01 <= 0) //frotal
 	{
+		//std::cout << "-------------------------------------------------------------------------------------------------------" << std::endl;
 		return true;
 	}
 	if (distantplaneX02 <= 0)//trasero
 	{
+		//std::cout << "-------------------------------------------------------------------------------------------------------" << std::endl;
 		return true;
 	}
 
 
 	if (distantplaneY01 <= 0)
 	{
+		if (i == 1)
+			std::cout << "-------------------------------------------------------------------------------------------------------" << std::endl;
 		return true;
 
 	}
 
 	if (distantplaneY02 <= 0)
 	{
+		//std::cout << "-------------------------------------------------------------------------------------------------------" << std::endl;
 		return true;
 	}
 
@@ -148,15 +182,17 @@ bool ColisionBox(const glm::vec3 preVertex, float dt)
 	////Rebotes Planos Laterales
 	if (distantplaneZ01 <= 0)//derecha
 	{
+		//std::cout << "-------------------------------------------------------------------------------------------------------" << std::endl;
 		return true;
 
 	}
 
 	if (distantplaneZ02 <= 0)//izquierda
 	{
+		//std::cout << "-------------------------------------------------------------------------------------------------------" << std::endl;
 		return true;
-
 	}
+
 	return false;
 }
 
@@ -166,22 +202,23 @@ void ApplyGravity(float dt) {
 	AngMom += dt*torque;
 	torque = glm::vec3(0);
 	Vel = LinMom / mass;
-	oldPos = Pos;
+	prePos = Pos;
 	Pos += dt *	Vel;
 	ITensorInv = glm::toMat3(rotQuat)*glm::inverse(IBody)*glm::transpose(glm::toMat3(rotQuat));
 	AngularVel = ITensorInv*LinMom;
+	preRotQuat = rotQuat;
 	rotQuat = glm::normalize(rotQuat + dt*(glm::quat(0, AngularVel)*rotQuat));
 }
 
-float BolzanoTime(float first, float last, const glm::vec3 vertex) {
+float BolzanoTime(float first, float last, const glm::vec3 vertex, int i) {
 	float mid = first + (last - first) / 2;
 
 	if (last - first > tolerancia) {
-		if (ColisionBox(vertex,mid)) {
-			last = BolzanoTime(first, mid, vertex);
+		if (ColisionBox(vertex,mid, i)) {
+			last = BolzanoTime(first, mid, vertex, i);
 		}
 		else {
-			first = BolzanoTime(mid, last, vertex);
+			first = BolzanoTime(mid, last, vertex, i);
 		}
 	}
 
@@ -191,4 +228,8 @@ float BolzanoTime(float first, float last, const glm::vec3 vertex) {
 	else {
 		return mid;
 	}
+}
+
+void rebote() {
+
 }
